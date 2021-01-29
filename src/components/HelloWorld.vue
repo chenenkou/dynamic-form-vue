47cec0650c91d5d312efff38f9ad0f9f5b4d6369
chenenkou <chenenkou@163.com>
2021-01-29 11:23:54 +0800

    0.1 update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@
             <el-row>
                 <el-col :span="8" :offset="8">
                     <div>
-                        <i-form :config="fields" :model="model" labelWidth="auto" label-suffix=":">
+                        <i-form :config="fields" :model="form" labelWidth="auto" label-suffix=":">
                             <el-row type="flex" justify="center">
                                 <el-button @click="onSubmit" type="primary">提交</el-button>
                             </el-row>
@@ -34,33 +34,75 @@
             return {
                 fields: {
                     name: {
-                        label: '用户名',
+                        label: '活动名称',
                         elTag: 'el-input',
-                        elAttrs: {
-                            placeholder: '请输入用户名'
-                        },
-                        customRender: 'nameCheck',
-                        rules: [{ required: true, message: '用户名不能为空', trigger: 'change' }]
+                        elAttrs: {}
                     },
-                    password: {
-                        label: '密码',
+                    region: {
+                        label: '活动区域',
+                        elTag: 'el-select',
+                        elAttrs: {
+                            placeholder: '请选择活动区域',
+                        },
+                        options: [
+                            {label: '区域一', value: 'shanghai'},
+                            {label: '区域二', value: 'beijing'},
+                        ],
+                    },
+                    date: {
+                        label: '活动日期',
+                        elTag: 'el-date-picker',
+                        elAttrs: {
+                            placeholder: '选择日期',
+                        },
+                    },
+                    delivery: {
+                        label: '即时配送',
+                        elTag: 'el-switch',
+                        elAttrs: {},
+                    },
+                    type: {
+                        label: '活动性质',
+                        elTag: 'el-checkbox-group',
+                        elAttrs: {},
+                        options: [
+                            {label: '美食/餐厅线上活动', value: 1},
+                            {label: '地推活动', value: 2},
+                            {label: '线下主题活动', value: 3},
+                            {label: '单纯品牌曝光', value: 4},
+                        ],
+                    },
+                    resource: {
+                        label: '特殊资源',
+                        elTag: 'el-radio-group',
+                        elAttrs: {},
+                        options: [
+                            {label: '线上品牌商赞助', value: 11},
+                            {label: '线下场地免费', value: 12},
+                        ],
+                    },
+                    desc: {
+                        label: '活动形式',
                         elTag: 'el-input',
                         elAttrs: {
-                            type: 'password',
-                            showPassword: true
-                        },
-                        rules: [{ required: true, trigger: 'change' }]
+                            type: "textarea"
+                        }
                     }
                 },
-                model: {
+                form: {
                     name: '',
-                    password: ''
+                    region: '',
+                    date: '',
+                    delivery: false,
+                    type: [],
+                    resource: '',
+                    desc: ''
                 }
             }
         },
         methods: {
             onSubmit() {
-                console.log('submit!', this.model);
+                console.log('submit!', this.form);
             }
         }
     }
--- src/components/form/Form.vue
+++ src/components/form/Form.vue
@@ -1,290 +1,88 @@
 <script>
-import is from 'is-type-of'
-import { toHyphenateEvent } from '../utils'
-import FormItem from './FormItem/Index'
+    import FormItem from './FormItem/Index'
 
-export default {
-  name: 'i-form',
-  components: { FormItem },
-  props: {
-    config: Object,
-    layout: Array,
-    disabled: Boolean
-  },
-  computed: {
-    rules() {
-      return Object.keys(this.config).reduce((acc, field, index) => {
-        acc[field] = this.config[field].rules
-        return acc
-      }, {})
-    },
-    elFormOptions() {
-      return this.$attrs
-    }
-  },
-  methods: {
-    _$render(a, b, c, d) {
-      return this.$createElement(a, b, c, d)
-    },
-    _setDefaultRule(field, rules = []) {
-      const { label } = this.config[field]
-      rules.forEach(rule => {
-        if (rule.required && !rule.message) {
-          rule.message = label + '不能为空'
-        }
-        if (!rule.trigger) {
-          rule.trigger = 'change'
-        }
-      })
-    },
-    getPathValue(path) {
-      const props = path.split('.')
-      if (props.length > 1) {
-        return props.reduce((model, prop, index) => {
-          if (index < props.length - 1 && typeof model == 'undefined') {
-            console.warn(`model.${props[index - 1]} 声明值错误，应为对象`)
-            model[prop] = {}
-          }
-          return model[prop]
-        }, this.elFormOptions.model)
-      }
-      return this.elFormOptions.model[path]
-    },
-    setPathValue(path, value) {
-      const props = path.split('.')
-      if (props.length > 1) {
-        props.reduce((model, prop, index) => {
-          if (index == props.length - 1) {
-            model[prop] = value
-          }
-          return model[prop]
-        }, this.elFormOptions.model)
-      } else {
-        this.elFormOptions.model[path] = value
-      }
-    },
-    visibleElFormItem(field) {
-      const { visible } = this.config[field]
-      const model = this.elFormOptions.model
-      if (typeof visible == 'undefined') {
-        return true
-      } else if (is.boolean(visible) && visible) {
-        return true
-      } else if (is.function(visible)) {
-        return visible({ model })
-      } else if (is.string(visible)) {
-        const modelVal = model[visible]
-        return !!(modelVal || modelVal === 0)
-      }
-      return false
-    },
-    /**
-     * 覆写 el-form validate
-     * @return Function(callback: Function(errors, model))
-     */
-    validate(cb) {
-      this.$refs.elFormRef.validate((valid, errors) => {
-        if (valid) {
-          cb(null, this.elFormOptions.model)
-        } else {
-          cb(errors, this.elFormOptions.model)
-        }
-      })
-    },
-    /**
-     * 覆写 el-form validateField
-     * @return Function(props, callback: Function(error))
-     */
-    validateField(props, cb) {
-      this.$refs.elFormRef.validateField(props, cb)
-    },
-    /**
-     * 覆写 el-form resetFields，扩展支持单个表单项清除
-     */
-    resetFields(props) {
-      if (!props) {
-        this.$refs.elFormRef.resetFields()
-      } else {
-        const fieldRefs = this.$refs.elFormRef.fields
-        const curFields = Array.isArray(props) ? props : [props]
-        curFields.forEach(curField => {
-          for (let i = 0; i < fieldRefs.length; i++) {
-            const field = fieldRefs[i].prop
-            if (field == curField) {
-              fieldRefs[i].resetField()
-            }
-          }
-        })
-      }
-    },
-    /**
-     * 覆写 el-form clearValidate
-     */
-    clearValidate(props) {
-      if (!props) {
-        this.$refs.elFormRef.resetFields()
-      } else {
-        this.$refs.elFormRef.clearValidate(Array.isArray(props) ? props : [props])
-      }
-    },
-    _validPathValue(path) {
-      const props = path.split('.')
-      let { model } = this.elFormOptions
-      if (props.length > 1) {
-        let isValid = true
-        let count = 0
-        let modelVal = null
-        while (count < props.length) {
-          modelVal = model[props[count]]
-          if (typeof modelVal === 'undefined') {
-            isValid = false
-            break
-          }
-          model = modelVal
-          count++
-        }
-        return typeof modelVal === 'undefined' ? false : true
-      } else {
-        return typeof model[path] === 'undefined' ? false : true
-      }
-    },
-    _isValidField(field) {
-      const fieldOptions = this.config[field]
-      if (!this._validPathValue(field)) {
-        console.warn(`model 未声明 ${field} 属性`)
-        return false
-      } else if (!fieldOptions) {
-        console.warn(`config 未设置 ${field} 配置`)
-        return false
-      }
-      return true
-    },
-    renderExtraTip(field) {
-      const { extra, extraIcon } = this.config[field]
-      if (extra) {
-        return this._$render('div', { class: 'extra-wrap' }, [this._$render('i', { class: extraIcon || 'el-icon-warning-outline' }), extra])
-      } else {
-        return ''
-      }
-    },
-    renderElFormItem(field) {
-      const { elTag, elAttrs, options, visible, customRender, extraRender, ...elFormItemOptions } = this.config[field]
-      const { rules } = elFormItemOptions
-      this._setDefaultRule(field, rules)
-      // https://element.eleme.io/#/zh-CN/component/form#form-item-attributes
-      const props = { ...elFormItemOptions, prop: field }
-      return this._$render('el-form-item', { props }, [this.renderFormItem(field), this.customRender(field), this.renderExtraTip(field)])
-    },
-    customRender(field) {
-      const { customRender } = this.config[field]
-      if (this.$scopedSlots && this.$scopedSlots[customRender]) {
-        return this.$scopedSlots[customRender]({ model: this.model })
-      }
-      return null
-    },
-    getSlotRender(field) {
-      const { slotRender } = this.config[field]
-      if (this.$scopedSlots && this.$scopedSlots[slotRender]) {
-        return this.$scopedSlots[slotRender]
-      }
-      return null
-    },
-    renderFormItem(field) {
-      const { elTag, changeMethod } = this.config[field]
-      if (!elTag) {
-        console.warn(`请设置 config[${field}].elTag`)
-        return ''
-      }
-      return this._$render('form-item', {
+    export default {
+        name: 'i-form',
+        components: {FormItem},
         props: {
-          field,
-          disabled: this.disabled,
-          value: this.getPathValue(field),
-          fieldOptions: this.config[field],
-          model: this.elFormOptions.model,
-          scopedSlotsFn: this.getSlotRender(field)
+            config: Object,
+            disabled: Boolean
         },
-        on: {
-          change: value => {
-            // this.elFormOptions.model[field] = value
-            this.setPathValue(field, value)
-            if (changeMethod) {
-              changeMethod.bind(this)({ value, model: this.elFormOptions.model, field, rules: this.rules })
+        computed: {
+            elFormOptions() {
+                // 包含了父作用域中不作为 props 被识别 (且获取) 的特性绑定 (class 和 style 除外)
+                return this.$attrs
             }
-          }
+        },
+        methods: {
+            // 渲染单个表单项元素
+            renderFormItem(field) {
+                const {elTag, changeMethod} = this.config[field]
+                if (!elTag) {
+                    console.warn(`请设置 config[${field}].elTag`)
+                    return ''
+                }
+                return this.$createElement('form-item', {
+                    props: {
+                        field,
+                        disabled: this.disabled,
+                        value: this.elFormOptions.model[field],
+                        fieldOptions: this.config[field],
+                        model: this.elFormOptions.model
+                    },
+                    on: {
+                        change: value => {
+                            this.elFormOptions.model[field] = value
+                            if (changeMethod) {
+                                changeMethod.bind(this)({
+                                    value,
+                                    model: this.elFormOptions.model,
+                                    field,
+                                    rules: this.rules
+                                })
+                            }
+                        }
+                    }
+                })
+            },
+            // 渲染单个表单项
+            renderElFormItem(field) {
+                const {elTag, elAttrs, options, visible, ...elFormItemOptions} = this.config[field]
+                // https://element.eleme.io/#/zh-CN/component/form#form-item-attributes
+                const props = {...elFormItemOptions, prop: field}
+                return this.$createElement('el-form-item', {props}, [this.renderFormItem(field)])
+            },
+            // 渲染表单所有项布局
+            drawLayout() {
+                const renders = []
+                for (let field in this.elFormOptions.model) {
+                    const formItemRender = this.renderElFormItem(field)
+                    renders.push(formItemRender)
+                }
+                return renders
+            },
+            // 渲染表单提交/取消...按钮
+            renderActionBar() {
+                if (this.$scopedSlots.default) {
+                    return this.$scopedSlots.default()
+                }
+                return ''
+            }
+        },
+        // 渲染表单
+        render(createElement) {
+            return createElement(
+                'el-form',
+                {
+                    props: this.elFormOptions,
+                    on: this.$listeners,
+                    ref: 'elFormRef'
+                },
+                [...this.drawLayout(), ...this.renderActionBar()]
+            )
         }
-      })
-    },
-    drawLayout(layout = []) {
-      const renders = []
-      if (layout.length > 0) {
-        for (const layoutOptions of layout) {
-          const { elTag, elAttrs, field, children = [] } = layoutOptions
-          if (elTag && !['el-row', 'el-col'].includes(elTag)) {
-            console.warn('elTag 建议为 el-row，el-col')
-          }
-          if (field) {
-            const fieldOptions = this.config[field]
-            if (!this._isValidField(field)) {
-              continue
-            }
-            if (!this.visibleElFormItem(field)) {
-              continue
-            }
-            const formItemRender = this.renderElFormItem(field)
-            if (elTag) {
-              renders.push(this._$render(elTag, { props: elAttrs }, [formItemRender]))
-            } else {
-              renders.push(formItemRender)
-            }
-          } else if (elTag) {
-            renders.push(this._$render(elTag, { props: elAttrs }, this.drawLayout(children)))
-          }
-        }
-      } else {
-        for (let field in this.elFormOptions.model) {
-          if (!this._isValidField(field)) {
-            continue
-          }
-          const fieldOptions = this.config[field]
-          if (!this.visibleElFormItem(field)) {
-            continue
-          }
-          const formItemRender = this.renderElFormItem(field)
-          renders.push(formItemRender)
-        }
-      }
-      return renders
-    },
-    renderActionBar() {
-      if (this.$scopedSlots.default) {
-        return this.$scopedSlots.default()
-      }
-      return ''
     }
-  },
-  render(h) {
-    console.log(this.elFormOptions)
-    return h(
-      'el-form',
-      {
-        props: this.elFormOptions,
-        on: toHyphenateEvent(this.$listeners),
-        ref: 'elFormRef'
-      },
-      [...this.drawLayout(this.layout), ...this.renderActionBar()]
-    )
-  }
-}
 </script>
 
 <style scoped>
-.extra-wrap {
-  font-size: 13px;
-  line-height: 1.5;
-  color: rgba(0, 0, 0, 0.45);
-}
-.extra-wrap i{
-    margin-right: 5px;
-}
 </style>
